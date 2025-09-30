5d986427fb8e3e22764302bf3843b81e168969f5
KTrordu <alptug.yilmaz@outlook.com>
2025-09-30 14:04:47 +0300

    finished challenge 2

--- Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,33 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    [SerializeField] private float dogSpawnTimerMax = 2.0f;
+
+    private float dogSpawnTimer;
+
+    private void Awake()
+    {
+        dogSpawnTimer = 0f;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (dogSpawnTimer > 0f) dogSpawnTimer -= Time.deltaTime;
+
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if (dogSpawnTimer <= 0f)
+            {
+                SpawnDog();
+            }
         }
+    }
+
+    private void SpawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        dogSpawnTimer = dogSpawnTimerMax;
     }
 }
